0da51585e01f478c68d705aa65aab64c79bac572
aisilu93 <aisilu93@yandex.ru>
2026-06-06 07:42:24 +0000

    Implement bounded_queue, test

--- include/queue/bounded_queue.hpp
+++ include/queue/bounded_queue.hpp
@@ -1,10 +1,21 @@
 #pragma once
 #include "queue/queue.hpp"
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <queue>
 
 namespace dispatcher::queue {
 
 class BoundedQueue : public IQueue {
-    // здесь ваш код
+    std::mutex mutex_;
+    int capacity = 0;
+    std::queue<std::function<void()>> tasks;
+
+    std::condition_variable cv_has_tasks;
+    std::condition_variable cv_ready_to_push;
+    std::atomic<bool> need_stop = false;
+
 public:
     explicit BoundedQueue(int capacity);
 
--- src/queue/bounded_queue.cpp
+++ src/queue/bounded_queue.cpp
@@ -1,7 +1,48 @@
 #include "queue/bounded_queue.hpp"
+#include <atomic>
+#include <condition_variable>
+#include <exception>
+#include <mutex>
+#include <stdexcept>
 
 namespace dispatcher::queue {
 
-// здесь ваш код
+BoundedQueue::BoundedQueue(int capacity) {
+    if (capacity <= 0)
+        throw std::invalid_argument("invalid capacity");
 
-} // namespace dispatcher::queue
+    this->capacity = capacity;
+}
+
+BoundedQueue::~BoundedQueue() {
+    need_stop.store(true, std::memory_order_release);
+    cv_has_tasks.notify_all();
+    cv_ready_to_push.notify_all();
+}
+
+void BoundedQueue::push(std::function<void()> task) {
+    std::unique_lock<std::mutex> cv_lock(mutex_);
+    cv_ready_to_push.wait(cv_lock,
+                          [this]() { return tasks.size() < capacity || need_stop.load(std::memory_order_acquire); });
+
+    if (need_stop.load(std::memory_order_acquire))
+        return;
+
+    tasks.emplace(std::move(task));
+    cv_has_tasks.notify_one();
+}
+
+std::optional<std::function<void()>> BoundedQueue::try_pop() {
+    std::unique_lock<std::mutex> cv_lock(mutex_);
+    cv_has_tasks.wait(cv_lock, [this]() { return !tasks.empty() || need_stop.load(std::memory_order_acquire); });
+    if (need_stop.load(std::memory_order_acquire))
+        return std::nullopt;
+
+    auto task = std::move(tasks.front());
+    tasks.pop();
+    cv_ready_to_push.notify_one();
+
+    return task;
+}
+
+}  // namespace dispatcher::queue
