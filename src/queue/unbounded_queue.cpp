7f55a0843c1223d0425cac89cb51030a93566e5a
aisilu93 <aisilu93@yandex.ru>
2026-06-06 11:45:30 +0000

    Implement unbounded queue, tests

--- src/queue/unbounded_queue.cpp
+++ src/queue/unbounded_queue.cpp
@@ -1,5 +1,5 @@
 #include "queue/unbounded_queue.hpp"
-
+#include <atomic>
 #include <functional>
 #include <mutex>
 #include <queue>
@@ -7,6 +7,51 @@
 
 namespace dispatcher::queue {
 
-// здесь ваш код
+UnboundedQueue::UnboundedQueue() {
+    options.capacity = 0;
+    options.bounded = false;
+}
 
-} // namespace dispatcher::queue
+UnboundedQueue::UnboundedQueue(int capacity) {
+    if (capacity <= 0)
+        throw std::invalid_argument("invalid capacity");
+
+    options.capacity = capacity;
+    options.bounded = true;
+}
+UnboundedQueue::~UnboundedQueue() {
+    need_stop.store(true, std::memory_order_release);
+    cv_has_tasks.notify_all();
+    cv_ready_to_push.notify_all();
+}
+
+void UnboundedQueue::push(std::function<void()> task) {
+    if (options.bounded) {
+        std::unique_lock<std::mutex> cv_lock(mutex_);
+        cv_ready_to_push.wait(
+            cv_lock, [this]() { return tasks.size() < options.capacity || need_stop.load(std::memory_order_acquire); });
+    }
+
+    if (need_stop.load(std::memory_order_acquire))
+        return;
+
+    tasks.emplace(std::move(task));
+    cv_has_tasks.notify_one();
+}
+
+std::optional<std::function<void()>> UnboundedQueue::try_pop() {
+    std::unique_lock<std::mutex> cv_lock(mutex_);
+    cv_has_tasks.wait(cv_lock, [this]() { return !tasks.empty() || need_stop.load(std::memory_order_acquire); });
+    if (need_stop.load(std::memory_order_acquire))
+        return std::nullopt;
+
+    auto task = std::move(tasks.front());
+    tasks.pop();
+
+    if (options.bounded)
+        cv_ready_to_push.notify_one();
+
+    return task;
+}
+
+}  // namespace dispatcher::queue
--- tests/queue/unbounded_queue.cpp
+++ tests/queue/unbounded_queue.cpp
@@ -1,5 +1,97 @@
+#include <functional>
 #include <gtest/gtest.h>
+#include <thread>
 
 #include "queue/unbounded_queue.hpp"
 
-// здесь ваш код
+using namespace dispatcher::queue;
+
+std::vector<int> v2;
+
+TEST(UnboundedCheck, IncorrectCapacityException) {
+    EXPECT_THROW(UnboundedQueue(-1), std::invalid_argument);
+    EXPECT_THROW(UnboundedQueue(0), std::invalid_argument);
+    EXPECT_NO_THROW(UnboundedQueue());
+    EXPECT_NO_THROW(UnboundedQueue(10));
+}
+
+TEST(UnboundedCheck, SimplePushAndPop) {
+    v2.clear();
+    UnboundedQueue queue;
+
+    auto in1 = [&]() { v2.push_back(1); };
+    auto in2 = [&]() { v2.push_back(2); };
+    auto in3 = [&]() { v2.push_back(3); };
+
+    queue.push(in1);
+    queue.push(in2);
+    queue.push(in3);
+
+    for (int i = 0; i < 3; i++) {
+        auto out = queue.try_pop();
+        EXPECT_TRUE(out.has_value());
+        std::invoke(out.value());
+        EXPECT_EQ(v2.back(), i + 1);
+    }
+}
+
+TEST(UnboundedCheck, PopFromEmptyWaits) {
+    v2.clear();
+    UnboundedQueue queue;
+
+    std::atomic<bool> wait_start(false);
+    std::atomic<bool> wait_end(false);
+
+    std::optional<std::function<void()>> out;
+    std::jthread try_pop_thread([&]() {
+        wait_start.store(true, std::memory_order_release);
+        out = queue.try_pop();
+        wait_end.store(true, std::memory_order_release);
+    });
+
+    sleep(1);
+    EXPECT_EQ(wait_start.load(std::memory_order_acquire), true);
+    EXPECT_EQ(wait_end.load(std::memory_order_acquire), false);
+    EXPECT_FALSE(out.has_value());
+
+    if (try_pop_thread.joinable()) {
+        queue.push([]() {});
+        try_pop_thread.join();
+    }
+}
+
+TEST(UnboundedCheck, PushToFullBoundedWaits) {
+    v2.clear();
+    UnboundedQueue queue(1);
+    std::atomic<bool> wait_start(false);
+    std::atomic<bool> wait_end(false);
+
+    auto in1 = []() { v2.push_back(1); };
+    auto in2 = []() { v2.push_back(2); };
+    queue.push(in1);
+
+    std::jthread try_push_thread([&]() {
+        wait_start.store(true, std::memory_order_release);
+        queue.push(in2);
+        wait_end.store(true, std::memory_order_release);
+    });
+
+    sleep(1);
+    EXPECT_EQ(wait_start.load(std::memory_order_acquire), true);
+    EXPECT_EQ(wait_end.load(std::memory_order_acquire), false);
+
+    auto out = queue.try_pop();
+
+    sleep(1);
+    EXPECT_EQ(wait_start.load(std::memory_order_acquire), true);
+    EXPECT_EQ(wait_end.load(std::memory_order_acquire), true);
+
+    out = queue.try_pop();
+    EXPECT_TRUE(out.has_value());
+    std::invoke(out.value());
+
+    EXPECT_EQ(v2.size(), 1);
+    EXPECT_EQ(v2[0], 2);
+    if (try_push_thread.joinable())
+        try_push_thread.join();
+}
